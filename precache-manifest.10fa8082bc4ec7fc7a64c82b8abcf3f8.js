bb4966edb26c83fcf2bae9db29ad74e7bf79a067
yeon94 <hjy9946@naver.com>
2020-10-07 17:03:25 +0900

    Updates

---
+++ precache-manifest.10fa8082bc4ec7fc7a64c82b8abcf3f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d51d783bc7ac11cfc4323986675afa1",
+    "url": "/yeon94/yeon_p/index.html"
+  },
+  {
+    "revision": "8f57445ac5f823ff8b94",
+    "url": "/yeon94/yeon_p/static/js/2.06f87be8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yeon94/yeon_p/static/js/2.06f87be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127379d9acd08740ec0e",
+    "url": "/yeon94/yeon_p/static/js/main.5250f039.chunk.js"
+  },
+  {
+    "revision": "9b167582a11c9b30ebfa",
+    "url": "/yeon94/yeon_p/static/js/runtime-main.2216c78b.js"
+  }
+]);
