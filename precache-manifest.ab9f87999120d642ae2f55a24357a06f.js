421c34e19f5f65b41129c7739284db0d6d126a89
yeon94 <hjy9946@naver.com>
2020-10-07 17:48:55 +0900

    Updates

---
+++ precache-manifest.ab9f87999120d642ae2f55a24357a06f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46810625180eec09047a6974e4df8a0e",
+    "url": "/yeon_p/index.html"
+  },
+  {
+    "revision": "41471635bc19e6254118",
+    "url": "/yeon_p/static/js/2.1c1c28c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yeon_p/static/js/2.1c1c28c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be18ffd7b531b691ee2e",
+    "url": "/yeon_p/static/js/main.d288e6fc.chunk.js"
+  },
+  {
+    "revision": "eb163abe7368a35ac067",
+    "url": "/yeon_p/static/js/runtime-main.7e3109c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yeon_p/precache-manifest.3a02ee013100b72fd0063986a2f4352d.js"
+  "/yeon_p/precache-manifest.ab9f87999120d642ae2f55a24357a06f.js"
 );
 
 self.addEventListener('message', (event) => {
