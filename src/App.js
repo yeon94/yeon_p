f8e23c1136df238685359ff6ac5088beb20d2156
yeon94 <hjy9946@naver.com>
2020-10-16 17:32:23 +0900

    title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Router from "./components/Router";
 import GlobalStyles from "./components/GlobalStyles";
+import Helmet from "react-helmet"
 
 class App extends React.Component {
 	render() {
 		return (
-			<>
+			<>	
+				<Helmet>
+					<title> Welcome | Yeon_P </title>
+				</Helmet>
 				<GlobalStyles />
 				<Router />
 			</>
